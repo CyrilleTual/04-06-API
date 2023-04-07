1d2a9ff69379b72448d2cd6b2fbccf412c3b504d
cyrilletual <cyrille.tual@gmail.com>
2023-04-07 10:46:36 +0200

    clean

--- js/search.js
+++ js/search.js
@@ -1,40 +1,42 @@
 import { toDisplay } from "./display.js";
 
-/// capture du champs de recherche:
 
-// Dune: Part Two
 const searchInput = document.getElementById("search");
 const API_KEY   = "9c6de8c116e01800dc9c56fe546028f4";
 const BASE_URL  = "https://api.themoviedb.org/3"
 const SEARCH  = "search";
 const MOVIES    = "movie";
+let arrayOfProducers =[];
 
+
+/**
+ * retourne un tableau en fonction de la saisie dans le champ de recherche
+ */
 function filmsToFind(toFind) {
     let urlReq1= `${BASE_URL}/${SEARCH}/${MOVIES}?api_key=${API_KEY}&query=${toFind}`
     fetch(urlReq1)
         .then (res => res.json())
         .then (datas => {
             // on passe le tableau de film à une seconde fonction 
-            // pour recherche de compléments
+            // pour recherche de compléments d'information
             addDetails(datas.results)
         })
 }
 
-// rechercher de la boite de prod et d'un lien vers celle-ci
-let arrayOfProducers =[];
 
-
-
+/**
+ * rechercher de la boite de prod et d'un lien vers celle-ci
+ */
 async function addDetails (arrayOfFilms){
-    
     let newArrayOfProducers;
     let newArrayOfFilms = [];
 
-    for await (const film of arrayOfFilms) {
+    for await (const film of arrayOfFilms) {  // pour chaque film de la selection
 
         newArrayOfProducers = []
-        // recupération des info boite de prod et lien 
+        // recupération d'un tableau des producteurs 
         await findProd(film.id)
+
         // pour chaque producteur on va chercher son website
         for await (const producer of arrayOfProducers) {
             let producerId = producer.id;
@@ -45,11 +47,11 @@ async function addDetails (arrayOfFilms){
         }
         // pour chaque film on ajoute une clef avec les producers en détail
         film.infoProducers = newArrayOfProducers;
-        // on reconstitue un nouveau tableau de films 
+        // on reconstitue un nouveau tableau de films complété 
         newArrayOfFilms.push(film);
     }
 
-    // appel de l'affichage
+    // appel de l'affichage avec le nouveau tableau de films
     toDisplay (newArrayOfFilms, true)
 
 }
@@ -64,7 +66,7 @@ async function findProd(idFilm){
         .then (film => {
             // recup des producteurs ( array)
             arrayOfProducers=film.production_companies; 
-            //film.production_companies; 
+            // film.production_companies; 
         })
 }
 
@@ -84,11 +86,9 @@ async function findWebSite(producerId){
 
 
 
-
-
-
-
-
+/**
+ * Ecouteur sur le champ de recherche 
+ */
 window.addEventListener('DOMContentLoaded', () => {
         // ecoute sur le texte au fur et à mesure de la frappe
         searchInput.addEventListener('keyup', function(e){
